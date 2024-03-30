e8eb025749ade76c0308e99516dcfcacb0931e7f
Nishimura <sigotoyou.mail00@gmail.com>
2024-03-30 11:30:09 +0900

    テストコミット

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,55 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<script setup>
+import { ref } from "vue";
+const test = ref(0);
+const userInputTask = ref("");
+const userIncompleteTasks = ref([]);
+const userCompleteTasks = ref([])
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+const add = (event) => {
+  console.log(event.pageX);
+  test.value++
+}
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+const taskAdd = () => {
+  if(userInputTask.value !== ""){
+    userIncompleteTasks.value.push(userInputTask.value)
+    userInputTask.value = ""
   }
 }
+
+const incompleteTaskDelete = (index) => userIncompleteTasks.value.splice( index, 1 )
+const completeTaskDelete = (index) => userCompleteTasks.value.splice( index, 1 )
+
+const taskComplete = (index) => {
+  userCompleteTasks.value.push(userIncompleteTasks.value[index])
+  userIncompleteTasks.value.splice( index, 1 )
+  console.log(userCompleteTasks.value)
+}
+
+const taskReturn = (index) => {
+  userIncompleteTasks.value.push(userCompleteTasks.value[index])
+  userCompleteTasks.value.splice( index, 1 )
+}
+
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <h1>Hello,World!</h1>
+  <h2>{{ test }}</h2>
+  <button @click="add">+1</button><br />
+  <input v-model="userInputTask" @keyup.enter="taskAdd" type="text"/><button @click="taskAdd">追加</button>
+  <p>やることリスト</p>
+  <li v-for="userIncompleteTask,index in userIncompleteTasks">
+    {{ userIncompleteTask }} 
+    <button @click="taskComplete(index)">完了</button>
+    <button @click="incompleteTaskDelete(index)">削除</button>
+  </li>
+  <p>終わったリスト</p>
+  <li v-for="userCompleteTask,index in userCompleteTasks">
+    {{ userCompleteTask }} 
+    <button @click="taskReturn(index)">戻す</button>
+    <button @click="completeTaskDelete(index)">削除</button>
+  </li>
+</template>
+
+<style scoped></style>
