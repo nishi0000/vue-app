7f798713297fdebc2111907730b36bba1254887f
Nishimura <sigotoyou.mail00@gmail.com>
2024-03-30 11:55:51 +0900

    テストコミット2

--- src/App.vue
+++ src/App.vue
@@ -39,13 +39,13 @@ const taskReturn = (index) => {
   <button @click="add">+1</button><br />
   <input v-model="userInputTask" @keyup.enter="taskAdd" type="text"/><button @click="taskAdd">追加</button>
   <p>やることリスト</p>
-  <li v-for="userIncompleteTask,index in userIncompleteTasks">
+  <li v-for="userIncompleteTask,index in userIncompleteTasks" :key="index">
     {{ userIncompleteTask }} 
     <button @click="taskComplete(index)">完了</button>
     <button @click="incompleteTaskDelete(index)">削除</button>
   </li>
   <p>終わったリスト</p>
-  <li v-for="userCompleteTask,index in userCompleteTasks">
+  <li v-for="userCompleteTask,index in userCompleteTasks" :key="index">
     {{ userCompleteTask }} 
     <button @click="taskReturn(index)">戻す</button>
     <button @click="completeTaskDelete(index)">削除</button>
