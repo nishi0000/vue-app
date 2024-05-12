82ff91d1a7867476fdb6a3c5bbdb79b6c557c0a2
Nishimura <sigotoyou.mail00@gmail.com>
2024-05-12 12:23:29 +0900

    CSS調整

--- src/pages/AppCreateToDo.vue
+++ src/pages/AppCreateToDo.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref } from "vue";
-import {db} from "../firebase_settings/index.js";
-import { addDoc, collection,serverTimestamp } from "firebase/firestore";
+import { db } from "../firebase_settings/index.js";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 
 const subject = ref("");
 const date = ref("");
@@ -9,19 +9,19 @@ const team = ref("");
 const pic = ref("");
 const detail = ref("");
 const status = ref("");
-
+const process = ref("");
 
 const onClickPost = async () => {
   await addDoc(collection(db, "todo"), {
-            subject:subject.value,
-            date:date.value,
-            team:team.value,
-            pic:pic.value,
-            detail:detail.value,
-            status:status.value,
-
-            timestamp: serverTimestamp(),
-});
+    subject: subject.value,
+    date: date.value,
+    team: team.value,
+    pic: pic.value,
+    detail: detail.value,
+    status: status.value,
+    process: process.value,
+    timestamp: serverTimestamp(),
+  });
 };
 </script>
 
@@ -30,11 +30,37 @@ const onClickPost = async () => {
 
   メール件名:<input v-model="subject" type="text" /><br>
   日付:<input v-model="date" type="text" /><br>
-  チーム:<input v-model="team" type="text" /><br>
+  <div class="radio-container">
+    <div>チーム:</div>
+    <div>
+      <input type="radio" name="team" value="A" v-model="team" />
+      <label for="teamA">A</label>
+    </div>
+    <div class="radio">
+      <input type="radio" name="team" value="B" v-model="team" class="radio" />
+      <label for="teamB">B</label>
+    </div>
+    <div>
+      <input type="radio" name="team" value="C" v-model="team" class="radio" />
+      <label for="teamA">C</label>
+    </div>
+    <div>
+      <input type="radio" name="team" value="D" v-model="team" class="radio" />
+      <label for="teamB">D</label>
+    </div>
+  </div>
   担当者:<input v-model="pic" type="text" /><br>
-  メール詳細:<textarea v-model="detail" type="text" /><br>
+  案件名:<input v-model="process" type="text" /><br>
+  メール詳細:<textarea v-model="detail" variant="standard" type="text" multiline /><br>
   ステータス:<input v-model="status" type="text" />
-  
-  
+
+
   <button @click="onClickPost">test</button>
 </template>
+
+
+<style scoped>
+.radio-container {
+  display: flex;
+}
+</style>
--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -1,34 +1,90 @@
 <script setup>
 import { ref, watchEffect } from 'vue';
-import {db} from "../firebase_settings/index.js";
+import { db } from "../firebase_settings/index.js";
 import { collection, getDocs } from "firebase/firestore";
 
-const loading = ref(true); 
+const loading = ref(true);
 const data = ref([])
 // ドキュメントを取得して表示
 watchEffect(async () => {
   const querySnapshot = await getDocs(collection(db, "todo"));
-  data.value = querySnapshot.docs.map(doc => doc.data()); 
+  data.value = querySnapshot.docs.map(doc => doc.data());
   console.log(data.value)
   loading.value = false
 })
 </script>
 
 <template>
+  <main>
+    <div class="title-container">
+      <h1>ToDoList</h1>
+      <p>A / B / C / D</p>
+      <p>すべて / 回答が必要 / 返事待ち / 完了</p>
+    </div>
 
-    <h1>ToDoList</h1>
-    <p>A / B / C / D</p>
-    <p>すべて / 回答が必要 / 返事待ち / 完了</p>
     <div v-if="loading">Loading...</div>
-    <ul v-for="data,index in data" :key="index">
-      <li>チーム{{data.team}} ステータス:{{ data.status }} 担当者:{{ data.pic }}</li>
-      <li>メール件名:{{ data.subject }} </li>
-      <li>日付:{{ data.date }} 〇日前</li>
-      <li>詳細:{{ data.detail }}</li>
-    </ul>
+
+    <div class="todo-container" v-for="data, index in data" :key="index">
+      <div class="tag-container">
+        <div class="tag">チーム{{ data.team }}</div>
+        <div class="tag">{{ data.status }}</div>
+        <div class="tag">{{ data.pic }}</div>
+        <div class="tag">{{ data.process }}</div>
+      </div>
+      <div class="mail-container">
+        <div class="mail-title">メール件名:{{ data.subject }} </div>
+        <div>日付:{{ data.date }}</div>
+        <div>〇日前</div>
+        <div class="detail-container">詳細:{{ data.detail }}</div>
+      </div>
+    </div>
 
 
-  <br />
-  <br />
-  <br />  <br />  <br />  <br />  <br />  <br />  <br />  <br />  <br />  <br />  <br />  <br />  <br />
+    <br />
+    <br />
+    <br /> <br /> <br /> <br /> <br /> <br />
+  </main>
 </template>
+
+
+<style scoped>
+.title-container {
+  width: 800px;
+  margin: 0 auto;
+}
+
+.todo-container {
+  /* background-color: aqua; */
+  margin: 12px auto;
+  padding: 12px;
+  width: 800px;
+}
+
+.tag-container {
+  display: flex;
+  gap: 8px;
+}
+
+.mail-container {
+  padding-left: 16px;
+}
+
+.tag {
+  font-size: small;
+  padding: 4px 8px;
+  border: 1px solid gray;
+  border-radius: 8px;
+}
+
+.mail-title {
+  margin-top: 8px;
+}
+
+.detail-container {
+  white-space: pre-line;
+}
+
+.todo-container:hover {
+  background-color: #f4f9ff;
+}
+</style>
